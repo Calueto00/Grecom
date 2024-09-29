7127feb3bf808fd009e6678472b60dbaaf7fc359
Paulo Calueto Francisco <paulo.calueto.francisco@gmail.com>
2024-09-29 11:53:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const slides = document.querySelectorAll('.item');
+let count = slides.length;
+const menuList = document.querySelector('.list');
+let activeSlider = 0;
+
+//* here comes my menu function
+function menu(e)
+{
+   e.name == 'menu' ? (e.name = 'close',menuList.classList.toggle('hidden')) : (e.name = 'menu',menuList.classList.toggle('hidden'));
+}
+//! ends over here my menu function
+
+//* here comes my slider functions
+next.onclick =()=>{
+    activeSlider += 1;
+        if(activeSlider >= count){
+            activeSlider = 0;
+        }
+        showSlides();
+}
+
+prev.onclick = ()=>{
+    activeSlider -= 1;
+    if(activeSlider < 0){
+        activeSlider = count -1;
+    }
+    showSlides();
+}
+
+function showSlides(){
+    let oldActiveSlider = document.querySelector(' .z-10');
+    oldActiveSlider.classList.remove('z-10');
+    slides[activeSlider].classList.add('z-10');
+}
+//! ends over here
+
+//* functions and methods to service modals
+
+const modalDomestica = document.querySelector('.modalDomestica');
+const modalIdoso = document.querySelector('.modalIdoso');
+const modalBaba = document.querySelector('.modalBaba');
+const closeModal = document.querySelector('.closeModal');
+
+function buttonDomestica(){
+    modalDomestica.classList.remove('hidden');
+}
+function closeModalDomestica(){
+    modalDomestica.classList.add('hidden');
+}
+
+function closeModalIdoso(){
+    modalIdoso.classList.add('hidden');
+}
+    closeModal.onclick = () =>{
+        modalBaba.classList.add('hidden');
+    }
+
+    function idosoModalButton(){
+        modalIdoso.classList.remove('hidden');
+    }
+function babaModalButton(){
+    modalBaba.classList.remove('hidden');
+}
+//!ends over here modals functions
+
+//*functions to send data do whatsapp
+
+function EnviarIdosoWhatsapp(){
+    let number = "+943888355";
+    const service = 'Cuidadora de Idosos';
+
+    let name = document.querySelector('#nameIdoso');
+    let location = document.querySelector('#locationIdoso');
+    let phone = document.querySelector('#telefoneIdoso');
+    let semana = document.querySelector('#semanaIdoso');
+    let services = document.querySelector('#serviceIdoso');
+    let needs = document.querySelector('#needIdoso');
+
+    let url = "https://wa.me/"+ number + "?text="+ "Serviço : "+ service +"%0a" 
+    + "name :" + name.value + "%0a" 
+     + "localização :" + location.value + "%0a"
+     + "Números de Idosos : " + phone.value + "%0a"
+     + "dias e horas de serviço : " + semana.value + "%0a"
+     + "Tipo de serviço: " + services.value + "%0a"
+     + "Necessidades do Idoso: " + needs.value + "%0a";
+
+    window.open(url,'_blank').focus();
+
+   name.value = '';
+   phone.value='';
+   location.value = '';
+   semana.value = '';
+   needs.value = '';
+   services.value = '';
+}
+//! ends over here
